3534b57aadea214b3859c561d710e35a25b66a16
Tahsinul Islam Rupom <tahsinulislam21@iut-dhaka.edu>
2025-04-29 21:22:41 +0600

    added hepler function, included item list

--- frontend/src/pages/ItemsPage.js
+++ frontend/src/pages/ItemsPage.js
@@ -13,6 +13,28 @@ const ItemPage = () => {
     useEffect(() => {
         fetchItems();
     }, []);
+
+    const fetchItems = async () => {
+        try {
+            setLoading(true);
+            const response = await fetch(`${API_BASE_URL}/api/items/get_items`, {
+                credentials: 'include'
+            });
+
+            if (!response.ok) {
+                throw new Error(`Error ${response.status}: ${response.statusText}`);
+            }
+
+            const data = await response.json();
+            setItems(data);
+            // setFilteredApartments(data);
+        } catch (err) {
+            setError(err.message || 'Failed to fetch items');
+            console.error('Error fetching items:', err);
+        } finally {
+            setLoading(false);
+        }
+    };
     return (
         <>
             <AppSidebar />
@@ -20,6 +42,29 @@ const ItemPage = () => {
                 <Typography variant="h4" component="h1" className="page-title" mb={6}>
                     Items for Sale
                 </Typography>
+
+                <Box className="main-container" width="100%" maxWidth="1200px" mb={4}>
+                    {/* Filter Section */}
+                    {/* <ApartmentFilters
+                        filters={filters}
+                        onFilterChange={handleFilterChange}
+                    /> */}
+
+                    {/* Apartment Listings */}
+                    {loading ? (
+                        <Box className="loading-container" mb={4}>
+                            <CircularProgress style={{ color: '#2d4f8f' }} />
+                        </Box>
+                    ) : error ? (
+                        <Alert severity="error" className="error-alert" mb={4}>
+                            {error}
+                        </Alert>
+                    ) : (
+                        <ItemsList
+                            items={items}//should be filteredItems after filters are done
+                        />
+                    )}
+                </Box>
             </Box>
         </>
     );
